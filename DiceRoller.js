a7010708f0a07929a525b0582d39e2029a899469
Thomas <tchaidez@iastate.edu>
2020-09-28 03:51:20 -0500

    Completed DiceRoller

---
+++ DiceRoller.js
@@ -0,0 +1,29 @@
+
+/*
+Using parts of Mr.Pogue's potpourri.js
+
+*/
+function RollDice1() {
+    document.getElementById("dice1").value = Math.floor(Math.random()*6) + 1;
+}
+
+function RollDice2() {
+    document.getElementById("dice2").value = Math.floor(Math.random()*6) + 1;
+}
+
+function RollDice3() {
+    document.getElementById("dice3").value = Math.floor(Math.random()*6) + 1;
+}
+
+function RollDice4() {
+    document.getElementById("dice4").value = Math.floor(Math.random()*6) + 1;
+}
+
+function RollDice5() {
+    document.getElementById("dice5").value = Math.floor(Math.random()*6) + 1;
+}
+
+function RollDice6() {
+    document.getElementById("dice6").value = Math.floor(Math.random()*6) + 1;
+}
+
